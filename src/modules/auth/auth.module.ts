797069dfda703927c9d2d80f402f6517b790ebc6
anitha13gb <gbriela.13rivera@gmail.com>
2024-10-29 00:34:43 -0500

    proyecto base

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
 
 @Module({
+  import:[
+    JwtModule.register({
+      secret: "Mi Codigo Secreto",
+      signOptions:{expiresIn: '60s' }
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
